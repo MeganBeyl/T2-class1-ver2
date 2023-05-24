3b15a1c00b332c6334512a8603e774dc11f3217c
Megan Beyl <231206@virtualwindow.co.za>
2023-05-24 14:20:30 +0200

    Done

---
+++ js/index.js
@@ -0,0 +1,10 @@
+console.log("Hello World")
+
+var firstName = "Megan";
+let surname = "Beyl";
+let age = 19;
+const subject = "Development";
+
+let message = "Hello " + firstName + " " + surname + ", You are " + age + " years old, and your subject is " + subject;
+
+console.log(message);
